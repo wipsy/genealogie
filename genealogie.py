19b2bab2656faca694ae15f0b3b537eeb8bb9d48
Votre Nom <benjforest@gmail.com>
2017-12-17 11:23:17 +0100

    First working commit

--- genealogie.py
+++ genealogie.py
@@ -10,27 +10,51 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import PathPatch
 import numpy as np
+import geojson
+import geopandas as gpd
+import mplleaflet
 
-fig     = plt.figure()
-ax      = fig.add_subplot(111)
+ancestors = gpd.GeoDataFrame.from_file('data/Genealogie.geojson')
+#france_map = gpd.GeoDataFrame.from_file('map-geojson/Metropolitan_France_AL6.GeoJson')
 
-map = Basemap( resolution='i', projection='tmerc', lat_0 = 46.227638, lon_0 = 2.213749000000007, width = 1000000, height = 1000000)
+print(ancestors)    
+ancestors['count'] = ancestors.groupby('birth place')['birth place'].transform('count')
+print(ancestors)
 
-map.drawmapboundary(fill_color='aqua')
-map.fillcontinents(color='lightgrey',lake_color='aqua')
-map.drawcoastlines()
+ancestors[1:10].plot(s=ancestors['count'][1:10]*100, column='birth', colormap='plasma')
 
-map.readshapefile('departements-20170102', 'france')
-print(map.france_info)
+#plt.scatter(ancestors['longitude'][1:10], ancestors['latitude'][1:10], s=ancestors['count'][1:10]*100, c=ancestors['birth'][1:10], cmap='plasma') # Draw blue line
 
-patches   = []
-for info, shape in zip(map.france_info, map.france):
-    if info['nom'] == 'Haute-Loire':
-        patches.append( Polygon(np.array(shape), True) )
+#fig, ax = plt.subplots()
+#ax.scatter(delta1[:-1], delta1[1:], c=close, s=volume, alpha=0.5)
+
+#print(weight['Bourbourg(59)'])
+
+#ax = genealogie.geometry.plot()
+#mplleaflet.display(fig=ax.figure) # To display it right at the notebook.
+mplleaflet.show() # To output _map.html file and display it in your browser.
+
+#fig     = plt.figure()
+#ax      = fig.add_subplot(111)
+
+#map = Basemap( resolution='i', projection='tmerc', lat_0 = 46.227638, lon_0 = 2.213749000000007, width = 1000000, height = 1000000)
+
+#map.drawmapboundary(fill_color='aqua')
+#map.fillcontinents(color='lightgrey',lake_color='aqua')
+#map.drawcoastlines()
+
+#map.readshapefile('shapefiles/departements-20170102', 'france')
+#print(map.france_info)
+
+
+#patches   = []
+#for info, shape in zip(map.france_info, map.france):
+    #poly = Polygon(np.array(shape), True)
+     #contains_point(point, transform=None, radius=0.0)¶
+    #if poly.contains((0, 0)):
+        #patches.append( poly )
         
-ax.add_collection(PatchCollection(patches, facecolor= 'm', edgecolor='w', linewidths=1., zorder=2))
+#ax.add_collection(PatchCollection(patches, facecolor= 'm', edgecolor='w', linewidths=1., zorder=2))
 
-x, y = map(0, 0)
-map.plot(x, y, marker='D',color='m')
-
-plt.show()
+#x, y = map(0, 0)
+#map.plot(x, y, marker='D',color='m')
